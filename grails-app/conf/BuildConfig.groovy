dc172478bd8ae0b984bae4a3c9815cb3b838577d
Burt Beckwith <burt@burtbeckwith.com>
2013-06-17 09:55:12 -0400

    use BuildConfig.groovy for dependencies instead of jars in the lib directory

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,6 +18,9 @@ grails.project.dependency.resolution = {
         compile 'org.apache.lucene:lucene-analyzers-common:4.1.0'
         compile 'redis.clients:jedis:2.0.0'
         compile 'com.trigonic:jedis-namespace:0.1'
+
+        compile 'com.belerweb:pinyin4j:2.5.0'
+        compile 'com.chenlb.mmseg4j:mmseg4j-core:1.9.0'
     }
 
     plugins {
